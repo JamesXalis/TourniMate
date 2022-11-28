de2e8724a3167eb06d5e82ceb3879d1abd116a41
James Xalis <jamesxalis@icloud.com>
2022-11-28 12:44:47 -0700

    added apollo server

--- Server/server.js
+++ Server/server.js
@@ -1,27 +1,32 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-const { Course } = require('./models');
-
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/all-courses', (req, res) => {
-    Course.find({}, (err, result) => {
-        if (err) {
-            res.status(500).send(err);
-        } else {
-            res.status(200).json(result);
-        }
-    });
-});
+const startApolloServer = async (typeDefs, resolvers) => {
+    await server.start();
+    server.applyMiddleware({ app });
+
+    db.once('open', () => {
+        app.listen(PORT, () => {
+            console.log(`API server running on port ${PORT}!`);
+            console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+        })
+    })
+};
 
 
-db.once('open', () => {
-    app.listen(PORT, () => {
-        console.log(`API server running on port ${PORT}!`);
-    });
-});
+startApolloServer(typeDefs, resolvers);
+
