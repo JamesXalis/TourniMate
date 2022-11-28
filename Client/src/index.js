bfba034e8a581efab07e3a5b3ff73479ee112c75
bmaggiano <brandon.maggiano@gmail.com>
2022-11-28 15:16:59 -0700

    fixed tournament index.js errors

--- Client/src/index.js
+++ Client/src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-reportWebVitals()
