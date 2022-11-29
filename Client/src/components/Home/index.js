80ceb7a7d9ab5419ae1fcd975707742241964f7b
Pogga1 <Andreweysoldt@gmail.com>
2022-11-29 10:38:18 -0800

    added carousel component

--- Client/src/components/Home/index.js
+++ Client/src/components/Home/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import'./home.css';
+import Carousel from '../Carousel';
 
 function Home() {
     return (
         <div className='Home'>
-            <h1>TourniMate</h1>
+            <h1 className=' header d-flex justify-content-center'>TourniMate</h1>
+        <div className='container d-flex just-content-around'>
+            <div className='gif'>
+            <img src='not_found.jpg'></img>
+            </div>
         </div>
-
+        <Carousel/>
+        </div>
     )
 }
 
