fd199dd2f4101478b7065c353bb013701466f799
Pogga1 <Andreweysoldt@gmail.com>
2022-12-01 12:25:32 -0800

    added .env file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 
 const cwd = process.cwd();
 
-const PORT = 3001;
+const PORT = process.env.PORT;
 const app = express();
 
 // Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
