c0f444b1dc7bf95f4919186a03a4187f8b814800
bmaggiano <brandon.maggiano@gmail.com>
2022-12-02 15:15:27 -0700

    added config stuff to connection

--- Server/Config/connection.js
+++ Server/Config/connection.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
 // Wrap Mongoose around local connection to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/aggregateDB', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/aggregateDB', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
 });
 
 // Export connection 
